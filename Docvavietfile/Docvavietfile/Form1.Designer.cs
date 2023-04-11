8fe68cb3172904222cfcef130eadc13a213c5b9e
shang9x123 <42996612+shang9x123@users.noreply.github.com>
2023-04-11 21:57:33 +0700

    đọc và ghi file ra txt
    đọc và ghi file ra txt

---
+++ Docvavietfile/Docvavietfile/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace Docvavietfile
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            listView1 = new ListView();
+            email = new ColumnHeader();
+            password = new ColumnHeader();
+            openFileDialog1 = new OpenFileDialog();
+            saveFileDialog1 = new SaveFileDialog();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(51, 99);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 0;
+            button1.Text = "Load file";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(51, 261);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 1;
+            button2.Text = "Save file";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // listView1
+            // 
+            listView1.Columns.AddRange(new ColumnHeader[] { email, password });
+            listView1.Location = new Point(339, 99);
+            listView1.Name = "listView1";
+            listView1.Size = new Size(396, 197);
+            listView1.TabIndex = 2;
+            listView1.UseCompatibleStateImageBehavior = false;
+            listView1.View = View.Details;
+            // 
+            // email
+            // 
+            email.Text = "email";
+            email.Width = 160;
+            // 
+            // password
+            // 
+            password.Text = "password";
+            password.Width = 160;
+            // 
+            // openFileDialog1
+            // 
+            openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(listView1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private ListView listView1;
+        private ColumnHeader email;
+        private ColumnHeader password;
+        private OpenFileDialog openFileDialog1;
+        private SaveFileDialog saveFileDialog1;
+    }
+}
---
+++ Docvavietfile/Docvavietfile/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System.Text;
+
+namespace Docvavietfile
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+
+                string[] lines = File.ReadAllLines(ofd.FileName);
+                foreach (string line in lines)
+                {
+                    System.Diagnostics.Debug.WriteLine(line);
+                    string[] array_line = line.Split('|');
+                    ListViewItem item = new ListViewItem() { Text = array_line[0] };
+                    item.SubItems.Add(array_line[1]);
+                    item.SubItems.Add(array_line[2]);
+                    listView1.Items.Add(item);
+                }
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.AddExtension = true;
+            saveFileDialog.RestoreDirectory = true;
+            saveFileDialog.Filter = "Text file(*.txt)|txt";
+            if(saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                // tìm đường dẫn và lưu lại
+                StreamWriter streamWriter = new StreamWriter(saveFileDialog1.FileName,false,Encoding.UTF8);
+                for (int i = 0; i < listView1.Items.Count; i++)
+                {
+                    ListViewItem item = listView1.Items[i]; 
+                    string line_insert = item.SubItems[0].Text +"|"+ item.SubItems[1].Text;
+                    streamWriter.WriteLine(line_insert);
+                } 
+                streamWriter.Close();
+                MessageBox.Show("Ghi thanh cong");
+                
+            }
+
+        }
+    }
+}
