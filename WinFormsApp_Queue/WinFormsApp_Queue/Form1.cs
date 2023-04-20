fb6e1d47666aeef9906aedf501dbfcffa35e1c6c
shang9x123 <42996612+shang9x123@users.noreply.github.com>
2023-04-21 00:06:28 +0700

    Queue
    Queue trong c sharp .

---
+++ WinFormsApp_Queue/WinFormsApp_Queue/Form1.cs
@@ -0,0 +1,29 @@
+﻿namespace WinFormsApp_Queue
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Queue<Person> listdanhsach  = new Queue<Person>();
+            listdanhsach.Enqueue(new Person { hoten = "hien" ,diachi="nam định"});
+            listdanhsach.Enqueue(new Person { hoten = "tuyet" ,diachi="thanh hóa"});
+            while(listdanhsach.Count > 0)
+            {
+                Person item = listdanhsach.Dequeue();
+                System.Diagnostics.Debug.WriteLine(item.hoten);
+                Thread.Sleep(5000);
+            }
+        }
+    }
+    class Person
+    {
+        public string hoten { get; set; }
+        public string diachi { get; set; }
+
+    }
+}
